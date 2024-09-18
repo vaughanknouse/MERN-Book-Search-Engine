f5017075412260fbfa26e1b300d6f0bdd0dcbf14
Vaughan Knouse <vaughanknouse@gmail.com>
2024-09-18 10:03:03 -0400

    added comment to vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
+      // Proxy requests to /graphql to your GraphQL server running at http://localhost:3001
       '/graphql': {
         target: 'http://localhost:3001',
         changeOrigin: true,
